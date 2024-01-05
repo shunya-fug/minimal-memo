34bf9d81491b13d29326cd52e5073868b6a3237d
shunya-fug <47451245+shunya-fug@users.noreply.github.com>
2024-01-05 12:26:58 +0000

    🔨：`@tailwindcss/typography`を適用

--- minimal-memo/src/app/page.tsx
+++ minimal-memo/src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <main className="container mx-auto">
-      <h1>title</h1>
-      <p>content</p>
+      <article className="prose">
+        <h1>title</h1>
+        <p>content</p>
+      </article>
     </main>
   );
 }
--- minimal-memo/tailwind.config.ts
+++ minimal-memo/tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
     themes: ["garden"],
   },
