3562cd116f3d98bef52cb2daf70dd002627520c4
shunya-fug <47451245+shunya-fug@users.noreply.github.com>
2024-01-09 13:31:44 +0000

    🗃️：テストデータ生成処理作成

---
+++ minimal-memo/prisma/seed.ts
@@ -0,0 +1,60 @@
+import { Prisma, PrismaClient, TagType } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const tweetList: Prisma.TweetCreateInput[] = [
+  {
+    sub: "sub1",
+    content: "test1",
+    tagList: {
+      create: [{ sub: "sub1", value: "#tag1", type: TagType.NORMAL }],
+    },
+  },
+  {
+    sub: "sub2",
+    content: "test2",
+    tagList: {
+      create: [{ sub: "sub2", value: "##folder2", type: TagType.FOLDER }],
+    },
+  },
+  {
+    sub: "sub3",
+    content: "test3",
+    tagList: {
+      create: [
+        { sub: "sub3", value: "#tag3/subtag3", type: TagType.NORMAL },
+        { sub: "sub3", value: "##folder3/subfolder3", type: TagType.FOLDER },
+      ],
+    },
+  },
+];
+
+const tagList: Prisma.TagCreateInput[] = [
+  {
+    sub: "sub",
+    value: "#folder",
+    type: TagType.NORMAL,
+  },
+  {
+    sub: "sub",
+    value: "##folder/subfolder",
+    type: TagType.FOLDER,
+  },
+];
+
+async function main() {
+  await prisma.tweet.deleteMany();
+  await prisma.tag.deleteMany();
+  for (const tweet of tweetList) {
+    await prisma.tweet.create({ data: tweet });
+  }
+  await prisma.tag.createMany({ data: tagList });
+}
+
+await main()
+  .catch((e) => {
+    console.error(e);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
