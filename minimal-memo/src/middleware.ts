0797a7c65b54200cd9f044d6ae6929546030333d
shunya-fug <47451245+shunya-fug@users.noreply.github.com>
2024-01-13 07:53:21 +0000

    🔨：認証処理をミドルウェアで行うよう修正

---
+++ minimal-memo/src/middleware.ts
@@ -0,0 +1,24 @@
+import { auth } from "@/util/auth";
+import { NextResponse } from "next/server";
+import { ApiErrorResponse } from "@/types/response";
+import { StatusCode } from "status-code-enum";
+
+export default auth(async (request) => {
+  // /api 以下のみ適用
+  if (!request.nextUrl.pathname.startsWith("/api")) {
+    return;
+  }
+
+  // /api/auth 以下は認証チェックしない
+  if (request.nextUrl.pathname.startsWith("/api/auth")) {
+    return;
+  }
+
+  // 認証チェック
+  if (!request.auth?.user?.id) {
+    return NextResponse.json<ApiErrorResponse>(
+      { message: "認証エラー" },
+      { status: StatusCode.ClientErrorUnauthorized }
+    );
+  }
+});
