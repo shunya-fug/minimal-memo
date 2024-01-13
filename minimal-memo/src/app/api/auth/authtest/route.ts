1634f10af3eb77c83b32ec2a79ef018d4126f035
shunya-fug <47451245+shunya-fug@users.noreply.github.com>
2024-01-13 08:01:23 +0000

    🔥：不要なファイルを削除

--- minimal-memo/src/app/api/auth/authtest/route.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/util/auth";
-
-export const GET = auth((request) => {
-  if (request.auth?.user) {
-    return Response.json("認証済み");
-  }
-  return Response.json("認証が必要");
-});
--- minimal-memo/src/app/api/authtest/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET(request: Request) {
-  return Response.json("認証不要");
-}
